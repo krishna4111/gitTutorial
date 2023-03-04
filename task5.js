9e0b517a99f5556109ba65e4b1790b09d3b0e234
krishnak4111 <krishnak4111@gmail.com>
2023-03-04 20:42:34 +0530

    add task5

---
+++ task5.js
@@ -0,0 +1,8 @@
+var sepli=document.getElementsByClassName("seperateli")[0];
+console.log(sepli)
+sepli.style.color='red'
+
+
+var tag=document.getElementsByTagName('li');
+console.log(tag[4])
+tag[4].style.fontWeight='bold';
